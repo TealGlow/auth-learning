59b8e4a3ba09d6e42a86c37ee0122751e62b6389
TealGlow <alyssacomstock95@gmail.com>
2022-09-12 09:54:16 -0700

    using mongoose for db auth tools instead to set ttl

--- db_auth_tools.js
+++ db_auth_tools.js
@@ -3,57 +3,47 @@ const { MongoClient } = require('mongodb')
 const mongoose = require('mongoose')
 
 const db_url = process.env.USER_DB_URL
-//const Client = new MongoClient(db_url);
 mongoose.connect(db_url)
-const schema = new mongoose.Schema({
+
+var db = mongoose.connection
+
+db.on('error', console.error.bind(console, "connection error:"))
+
+const RefreshSchema = new mongoose.Schema({
     token: {type: String},
-    createdAt: {type: Date, expires:'1m', index:true, default: Date.now},
+    createdAt: {type: Date, expires:'720m', index: true, default: Date.now},
 })
 
-const refresh_token = mongoose.model('refresh_token', schema, 'user_accounts')
-//const myDb = mongoose.connection.model('refresh_token_cache', schema)
-//const myDb = Client.db("user_accounts").collection("refresh_token_cache")
+var refresh_token = mongoose.model('RefreshToken', RefreshSchema, 'refresh_token_cache')
+
+
 
 exports.addRefreshToken = async function(token){
   const result = new refresh_token({token:token})
   result.save((err, res)=>{
     if(err) console.log(err)
-    console.log("done")
+    console.log("Refresh token added!")
   })
-  /*const result = await myDb.insertOne({
-    token: token,
-    createdAt: new Date(),
-  })*/
-
-  //await Client.close()
   return
 }
 
 
-exports.getRefreshToken = async function(token){
-  //await Client.connect()
-  const result = await myDb.findOne({token:token})
 
-  //await Client.close()
-  return result
+exports.getRefreshToken = async function(token){
+  return await refresh_token.findOne({token:token})
 }
 
 
-exports.deleteRefreshToken = async function(token){
-  await Client.connect()
 
-  const result = await myDb.deleteMany({token:token})
-  await Client.close()
+exports.deleteRefreshToken = async function(token){
+  const result = await refresh_token.deleteMany({token:token})
   if(result.deletedCount>0) return 204
   return 404
 }
 
+
+
 exports.getRefreshTokens = async function(){
-  await Client.connect()
-
-  const result = await myDb.find().toArray()
-  console.log(result)
-
-  await Client.close()
+  const result = await refresh_token.find().toArray()
   return result
 }
